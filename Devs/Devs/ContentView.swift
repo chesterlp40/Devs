eccdd6e435788fd954b1e7b24258d18e5b4e8b6d
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-06-07 23:24:21 -0300

    Start project

---
+++ Devs/Devs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Devs
+//
+//  Created by Ezequiel Rasgido on 07/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Devs/Devs/DevsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevsApp.swift
+//  Devs
+//
+//  Created by Ezequiel Rasgido on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DevsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
